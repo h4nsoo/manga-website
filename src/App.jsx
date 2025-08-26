c06fbc6b649492808847aabe5267bd9334427994
h4nsoo <mbhansoo05@gmail.com>
2025-08-26 11:59:41 +0100

    modern scroll bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useRef } from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
@@ -7,29 +8,39 @@ import ChapterReaderPage from "./pages/ChapterReaderPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import PopularPage from "./pages/PopularPage";
 import Footer from "./components/Footer";
+import SimpleBar from "simplebar-react";
+import "simplebar-react/dist/simplebar.min.css";
 import "./styles/App.css";
 
 function App() {
+  const simpleBarRef = useRef(null);
+
   return (
     <div className="app-container">
       <Navbar />
+      <SimpleBar
+        className="custom-scrollbar"
+        autoHide={true}
+        forceVisible={false}
+        timeout={1500}
+        ref={simpleBarRef}
+      >
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/browse" element={<BrowsePage />} />
+            <Route path="/popular" element={<PopularPage />} />
+            <Route path="/manga/:id" element={<MangaDetailPage />} />
+            <Route
+              path="/manga/:mangaId/chapter/:chapterId"
+              element={<ChapterReaderPage />}
+            />
 
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/browse" element={<BrowsePage />} />
-          <Route path="/popular" element={<PopularPage />} />
-          <Route path="/manga/:id" element={<MangaDetailPage />} />
-          <Route
-            path="/manga/:mangaId/chapter/:chapterId"
-            element={<ChapterReaderPage />}
-          />
-
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
-
-      <Footer />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+        <Footer />
+      </SimpleBar>
     </div>
   );
 }
